c6b5b598b00e31659b912b9e6f0b04ea3cb75cf6
Namchok <pech4751@gmail.com>
2020-03-19 17:48:04 +0700

    Add login facebook

--- app/src/main/java/com/example/projectteam/FragmentMemberOfTeam.kt
+++ app/src/main/java/com/example/projectteam/FragmentMemberOfTeam.kt
@@ -5,18 +5,66 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.facebook.login.LoginManager
 
 /**
  * A simple [Fragment] subclass.
  */
 class FragmentMemberOfTeam : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_member_of_team, container, false)
+    var PhotoURL : String = ""
+    var Name : String = ""
+
+    fun newInstance(url: String,name : String): FragmentMemberOfTeam {
+
+        val profile = FragmentMemberOfTeam()
+        val bundle = Bundle()
+        bundle.putString("PhotoURL", url)
+        bundle.putString("Name", name)
+        profile.setArguments(bundle)
+
+        return profile
     }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment_member_of_team, container, false)
+
+        val ivProfilePicture = view.findViewById(R.id.iv_profile) as ImageView
+        val tvName = view.findViewById(R.id.tv_name) as TextView
+        val login_button2 = view.findViewById(R.id.login_button2) as Button
+
+        Glide.with(activity!!.baseContext)
+            .load(PhotoURL)
+            .into(ivProfilePicture)
+
+        tvName.setText(Name)
+
+        login_button2.setOnClickListener{
+
+            LoginManager.getInstance().logOut()
+            activity!!.supportFragmentManager.popBackStack()
+        }
+        // Inflate the layout for this fragment
+        return view
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val bundle = arguments
+        if (bundle != null) {
+            PhotoURL = bundle.getString("PhotoURL").toString()
+            Name = bundle.getString("Name").toString()
+
+        }
+
+    }
+
 
 }
--- app/src/main/java/com/example/projectteam/MainActivity.kt
+++ app/src/main/java/com/example/projectteam/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.example.projectteam
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
 
+
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
         setContentView(R.layout.activity_main)
+
+        val fmAuthen = Authen()
+        supportFragmentManager.beginTransaction().replace(R.id.layout, fmAuthen, "fmAuthen").addToBackStack("fmAuthen").commit()
+
+        debugHashKey()
+    }
+
+    @SuppressLint("PackageManagerGetSignatures")
+    private fun debugHashKey() {
+        try {
+            val info = packageManager.getPackageInfo(
+                "com.example.projectteam",
+                PackageManager.GET_SIGNATURES
+            )
+            for (signature in info.signatures) {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.d("KeyHash:", Base64.getEncoder().encodeToString(md.digest()))
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+        } catch (e: NoSuchAlgorithmException) {
+        }
     }
 }
