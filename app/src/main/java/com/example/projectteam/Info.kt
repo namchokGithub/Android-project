65bdd588784d39c994fc4b022676325477ecd361
Namchok <pech4751@gmail.com>
2020-03-19 14:49:26 +0700

    Add activity

---
+++ app/src/main/java/com/example/projectteam/Info.kt
@@ -0,0 +1,12 @@
+package com.example.projectteam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Info : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+    }
+}
